74536197f64f8dcf835ef015620d05845b1d2d67
sahiltemp92-cyber <sahiltemp92@gmail.com>
2026-09-20 03:04:12 +0530

    updated extraction test

--- tests/Client.spec.js
+++ tests/Client.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test')
 
-test.only('Extract text content - Login Test with Locators', async ({ page }) => {
+test.only('Extract text content - Test with Locators', async ({ page }) => {
     // const context = await browser.newContext();
     // const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
@@ -8,18 +8,22 @@ test.only('Extract text content - Login Test with Locators', async ({ page }) =>
     await page.locator("[id='userEmail']").fill("sahil.khenat.career@gmail.com")
     await page.locator("[id='userPassword']").fill("Rahul@12345")
     await page.locator("//input[@name='login']").click();
+    await page.waitForLoadState("domcontentloaded"); // Wait till the page is loaded completely
     const sign_out_button = page.getByRole("button", { name: "Sign Out" });
+
     await expect(sign_out_button).toBeVisible();
+
+
+    // Get all products names and verify ADIDAS ORIGINAL is in it
+    const all_products = page.locator(".card-body b");
+    const all_products_names = (await all_products.allTextContents());
+    console.log(all_products_names);
+    expect(all_products_names).toContain("ADIDA1S ORIGINAL");
 
     // Get first product name and verify it contains ADIDAS ORIGINAL
     const first_product = page.locator(".card-body b").first();
     const first_product_name = (await first_product.textContent());
-    await expect(first_product_name).toContain('ADIDAS ORIGINAL')
+    expect(first_product_name).toContain('ADIDAS ORIGINAL');
     console.log(first_product_name);
-
-    // Get all products names
-    const all_products = page.locator(".card-body b");
-    const all_products_names = (await all_products.allTextContents());
-    console.log(all_products_names);
 }
 )
